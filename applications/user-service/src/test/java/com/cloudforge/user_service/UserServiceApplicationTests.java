825897cbcabf1ff3add7d6a2f4a6611c29036b98
Akila Isuri <akilaisuri98@gmail.com>
2026-07-18 08:55:33 +0530

    feat(user-service): initialize Spring Boot project and add home endpoint

---
+++ applications/user-service/src/test/java/com/cloudforge/user_service/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudforge.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
