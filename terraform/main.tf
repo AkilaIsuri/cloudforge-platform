4e239c63e8ad6d2b5bf6b4ba7b5cf29fc4a11908
Akila Isuri <akilaisuri98@gmail.com>
2026-08-30 12:13:16 +0530

    Updating route table associations

--- terraform/main.tf
+++ terraform/main.tf
@@ -122,3 +122,14 @@ module "eks" {
     module.private_subnet_b.subnet_id
   ]
 }
+
+
+resource "aws_route_table_association" "private_a" {
+  subnet_id      = module.private_subnet_a.subnet_id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private_b" {
+  subnet_id      = module.private_subnet_b.subnet_id
+  route_table_id = aws_route_table.private.id
+}
